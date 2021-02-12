6b6abf539a686785217a232d6cfa2e4a688b47d1
lsmucassi <lindasmucassi@gmail.com>
2021-02-12 19:40:19 +0200

    login: layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-        );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            child: LoginPage(),
+          ),
+        ));
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+              child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 100),
+                child: Text("crafta"),
+              ),
+              Text("we are crafted, we are Art"),
+            ],
+          )),
+          Container(
+              child: Center(
+            child: Text("Image"),
+          )),
+          Container(
+              child: Center(
+            child: Text("Button"),
+          )),
+        ],
+      ),
+    );
   }
 }
